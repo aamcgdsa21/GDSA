0d76dd155911517f04fb3465265e300cc3f38424
aamcgdsa21 <annasalo43@gmail.com>
2014-12-21 12:14:12 +0100

    Update avaluador.m

--- avaluador/avaluador.m
+++ avaluador/avaluador.m
@@ -14,12 +14,12 @@ fclose(FID1);
 id=VT{:,1}; % A la primera columna hi han les ids de les imatges
 event=VT{:,2};% A la segona columna hi ha l'event al que pertany cada id
 event=event';
-LVeritat=length(event); %Obtenim la llargada del vector que contÈ la veritat terreny. 
+LVeritat=length(event); %Obtenim la llargada del vector que cont√© la veritat terreny. 
  
 idC=C{:,1}; % A la primera columna hi han les ids de les imatges
 eventC=C{:,2}; % A la segona columna hi ha l'event al que pertany cada id
 eventC=eventC';
-LClas=length(eventC); %Obtenim la llargada del vector que contÈ les imatges classificades.
+LClas=length(eventC); %Obtenim la llargada del vector que cont√© les imatges classificades.
  
 
  
@@ -32,7 +32,7 @@ end
  
 for i=1:LClas
     idm = idC{i};
-    if(isKey(map_veritat,idm) == 1) %Si l'id existeix a la veritat terreny l'agafem, sinÛ no fem res
+    if(isKey(map_veritat,idm) == 1) %Si l'id existeix a la veritat terreny l'agafem, sin√≥ no fem res
         id{i} = idm;
         event{i} = map_veritat(idm);
     end    
@@ -41,7 +41,7 @@ end
  
 %per utilitzar la funcio "getcm" que genera parametres d'avaluacio, els vectors de clases de les imatges i la
 %veritat terreny han d'estar classificades en enters
-%strcmp compara si la posiciÛ del vector es la mateixa que fashion, non-event....
+%strcmp compara si la posici√≥ del vector es la mateixa que fashion, non-event....
 %generem el vector d'enters del Veritat terreny
 v = zeros(1,LVeritat); %l'emplenem de 0 perque els dos vectors v i c d'enters siguin de la mateixa llargaria.
 for i=1:LVeritat
@@ -94,46 +94,64 @@ for i=1:LClas
 end;
 %vector=1:9;
 %cridem a la funcio que ens dona els resultats
-[mconfusio,numcorrecte,precisio,recall,fscore] = getcm(v,c,(1:9))
+[mconfusio,numcorrecte,precisio,record,fscore] = getcm(v,c,(1:9))
 %Contem quantes classes hi ha a la veritat terreny i al classificador
 cont = 0;
 for j=1:9
     for i=1:length(v)
         if v(i)==j
             cont = cont + 1;
-            break; %si troba la classe surt del for i va a la seguent classe del classificador per mirar si est‡ tambÈ en el vector veritat terreny
+            break; %si troba la classe surt del for i va a la seguent classe del classificador per mirar si est√† tamb√© en el vector veritat terreny
         end
     end
 end
  
 %calculs de les mitjanes 
 Precisio = sum(precisio)/cont;
-Recall = sum(recall)/cont;
+Record= sum(record)/cont;
 Fscore = sum(fscore)/cont;
 Exactitud=(numcorrecte/LClas)*100;
 
+ordre_events = {'fashion','concert','non_event','exhibition','sports','protest','theater_dance','conference','other'};
  
- 
-%creaciÛ del fitxer de resultats
+%creaci√≥ del fitxer de resultats
 fid=fopen('resultats.txt','w');
+
+fprintf(fid, 'Precisi√≥:');
+for i=1:9
+    fprintf(fid,'%f \n',precisio(i));
+    fprintf(fid, '\n');
+end
+
+fprintf(fid, 'Record:');
+for i=1:9
+    fprintf(fid,'%f \n', record(i));
+    fprintf(fid, '\n');
+end
+
+fprintf(fid, 'Fscore:\n');
+for i=1:9
+    fprintf(fid,'%f \n',fscore(i));
+    fprintf(fid, '\n');
+end
  
-fprintf(fid,'PrecisiÛ=');
+fprintf(fid,'Precisi√≥=');
 fprintf(fid,'%f \n',Precisio);
 fprintf(fid,'\n');
  
-fprintf(fid,'Recall=');
-fprintf(fid,'%f \n',Recall);
+fprintf(fid,'Record=');
+fprintf(fid,'%f \n',Record);
 fprintf(fid,'\n');
  
 fprintf(fid,'Fscore=');
 fprintf(fid,'%f \n',Fscore);
 fprintf(fid,'\n');
- 
+Exactitut= Exactitud/100;
 fprintf(fid,'Exactitud=');
-fprintf(fid,'%f \n',Exactitud);
+fprintf(fid,'%f \n',Exactitut);
 fprintf(fid,'\n');
 
-fprintf(fid,'Matriu de confusiÛ=');
+fprintf(fid,'Matriu de confusi√≥=');
 fprintf(fid,'%f \n');
 fprintf(fid,'\n');
 
@@ -149,7 +167,21 @@ end;
  
 fclose(fid);
 
- 
+figure(1),
+x=[precisio(1),precisio(2),precisio(3),precisio(4),precisio(5),precisio(6),precisio(7),precisio(8),precisio(9)];
+bar(x,'green'), title('Precisi√≥');
+ylabel('resultat')
+xlabel('tipus de classe')
+figure(2),
+x2=[record(1),record(2),record(3),record(4),record(5),record(6),record(7),record(8),record(9)];
+bar(x2,'blue'), title('Record');
+ylabel('resultat')
+xlabel('tipus de classe')
+figure(3),
+x3=[fscore(1),fscore(2),fscore(3),fscore(4),fscore(5),fscore(6),fscore(7),fscore(8),fscore(9)];
+bar(x3,'r'), title('Fscore');
+ylabel('resultat')
+xlabel('tipus de classe')
  
 
 
